eae2ca24abb3a81385e65847d7f7f3e9859ed7e4
gomafuu <a.akane.0207.trb@gmail.com>
2023-09-22 05:16:03 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,60 @@
+class PrototypesController < ApplicationController
+
+  before_action :authenticate_user!,only:[:create,:update,:destroy]
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.create(prototype_params)
+    # prototype_paramsをしたで呼び出す
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @prototype = Prototype.create(prototype_params)
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  
+  private
+
+  def prototype_params
+    params.require(:prototype).
+    permit(:title, :catch_copy, :concept, :image, :name).
+    merge(user_id: current_user.id)
+  end
+end
---
+++ db/migrate/20230918045400_create_comments.rb
@@ -0,0 +1,15 @@
+class CreateComments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+
+      t.timestamps
+      t.text :content,             null:false
+
+      t.references :user, null: false, foreign_key: true
+      t.references :prototype, null: false, foreign_key: true
+
+      belongs_to :users
+      belongs_to :prototypes
+    end
+  end
+end
